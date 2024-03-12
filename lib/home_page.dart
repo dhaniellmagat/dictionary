aac226cf62108fe447546f32050100659821a140
dhaniellmagat <156490795+dhaniellmagat@users.noreply.github.com>
2024-03-12 23:50:59 +0800

    Update home_page.dart

---
+++ lib/home_page.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+import 'package:project_app/api.dart';
+import 'package:project_app/response_model.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool inProgress = false;
+  ResponseModel? responseModel;
+  String noDataText = "Welcome, to our Dictionary!";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[100], // Use a light background color
+      child: SafeArea(
+        child: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildHeaderWidget(), // Added header widget
+                _buildSearchWidget(),
+                const SizedBox(height: 12),
+                if (inProgress)
+                  const LinearProgressIndicator()
+                else if (responseModel != null)
+                  Expanded(child: _buildResponseWidget())
+                else
+                  _noDataWidget(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildHeaderWidget() {
+    return const Center(
+      child: Padding(
+        padding: EdgeInsets.only(bottom: 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.book,
+              color: Colors.black87,
+              size: 30,
+            ),
+            SizedBox(width: 8), // Add some spacing between the icon and text
+            Text(
+              "Dictionary",
+              style: TextStyle(
+                color: Colors.black87,
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+  _buildResponseWidget() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 16),
+        Text(
+          responseModel!.word!,
+          style: TextStyle(
+            color: Colors.purple.shade600,
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
+        ),
+        Text(responseModel!.phonetic ?? ""),
+        const SizedBox(height: 16),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return _buildMeaningWidget(responseModel!.meanings![index]);
+            },
+            itemCount: responseModel!.meanings!.length,
+          ),
+        ),
+      ],
+    );
+  }
+
+  _buildMeaningWidget(Meanings meanings) {
+    String definitionList = "";
+    meanings.definitions?.forEach(
+          (element) {
+        int index = meanings.definitions!.indexOf(element);
+        definitionList += "\n${index + 1}. ${element.definition}\n";
+      },
+    );
+
+    return Card(
+      color: Colors.grey[200], // Use a light background color
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              meanings.partOfSpeech!,
+              style: TextStyle(
+                color: Colors.orange.shade600,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 12),
+            const Text(
+              "Definitions : ",
+              style: TextStyle(
+                color: Colors.black87,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+            Text(definitionList.trim()), // Remove leading/trailing whitespaces
+            _buildSet("Synonyms", meanings.synonyms),
+            _buildSet("Antonyms", meanings.antonyms),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _buildSet(String title, List<String>? setList) {
+    if (setList?.isNotEmpty ?? false) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "$title : ",
+            style: const TextStyle(
+              color: Colors.black87,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(setList!.join(', ')), // Use a comma-separated list
+          const SizedBox(height: 10),
+        ],
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+
+  _noDataWidget() {
+    return SizedBox(
+      height: 100,
+      child: Center(
+        child: Text(
+          noDataText,
+          style: const TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+
+  _buildSearchWidget() {
+    return SearchBar(
+      hintText: "Type here the word you want to know about",
+      onSubmitted: (value) {
+        _getMeaningFromApi(value);
+      },
+    );
+  }
+
+  _getMeaningFromApi(String word) async {
+    setState(() {
+      inProgress = true;
+    });
+    try {
+      responseModel = await API.fetchMeaning(word);
+      setState(() {});
+    } catch (e) {
+      responseModel = null;
+      noDataText = "Meaning cannot be fetched";
+    } finally {
+      setState(() {
+        inProgress = false;
+      });
+    }
+  }
+}
